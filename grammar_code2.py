9a0a8266498bd596a18afeae567e22e85637f675
ngoc anh <anhptnk24406@st.uel.edu.vn>
2025-03-19 22:24:59 +0700

    Lưu thay đổi trước khi chuyển nhánh

--- grammar_code2.py
+++ grammar_code2.py
@@ -1,4 +1,8 @@
 from PyQt6.QtWidgets import QTabBar, QTableWidget, QTableWidgetItem, QMessageBox
-from Product_UI import Ui_MainWindow
+from grammar import Ui_MainWindow
 from pymongo import MongoClient
 from bson import ObjectId
+#MongoDB Connection
+client=MongoClient("mongodb://localhost:27017/")
+db=client["exam_db"]
+collection=db["beers"]
